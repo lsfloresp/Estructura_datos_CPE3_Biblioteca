ba2bc4287b55d1ba67aa40c1b59a8815776359fb
lsfloresp <ls.floresp@uea.edu.ec>
2026-09-02 23:53:28 -0500

    Implementacion inicial del sistema de biblioteca

--- Libro.cs
+++
@@ -1,8 +0,0 @@
-public class Libro
-{
-    public int Codigo { get; set; }
-    public string Titulo { get; set; }
-    public string Autor { get; set; }
-    public string Categoria { get; set; }
-    public int Ejemplares { get; set; }
-}
